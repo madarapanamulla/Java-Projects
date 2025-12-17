c68bd0998a9e88717f42d5b428425aa38c685705
Madara Panamulla <panamullamadara@gmail.com>
2025-12-17 22:28:31 +0200

    Done for today

--- exercise.java
+++ exercise.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
+
 public class exercise {
     public static void main(String[] args){
 
         //System.out.println(Math.PI);
         //System.out.println(Math.E);
 
+        Scanner scanner = new Scanner(System.in);
 
     double result;
+    double a;
+    double b;
+    double c;
 
     result = Math.pow(2.0, 5.0);
     result = Math.sqrt(25);
@@ -18,5 +25,19 @@ public class exercise {
 
 
     System.out.println(result);
+
+    System.out.println("Enter the leangth of side A: ");
+    a = scanner.nextDouble();
+
+    System.out.println("Enter the lenght of side B: ");
+    b = scanner.nextDouble();
+
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+    System.out.println("The lenght of the side C is: " + c);
+
+        scanner.close();
+
+
     }
 }
